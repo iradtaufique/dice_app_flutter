87818dfb005b68d7d815f9231079c6b8cecab019
Taufique Iradukunda <taufiqueira@Taufiques-MacBook-Pro.local>
2024-01-06 23:44:47 -0500

    change from stateless widget to statefull widget to allow button to change the state everytime it is clicked

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,15 @@ void main() {
   );
 }
 
-// Stateless class DicePage
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  // const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int lefDiceNum = 3;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -29,10 +36,18 @@ class DicePage extends StatelessWidget {
         children: [
           Expanded(
             child: TextButton(
-              onPressed: () {},
-              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('dice right pressed = $lefDiceNum');
+                // sateState method used to update or run the build method again and update it with new value
+                setState(() {
+                  lefDiceNum = 1;
+                  print('dice right pressed');
+                });
+              },
+              child: Image.asset('images/dice$lefDiceNum.png'),
             ),
           ),
+          // this will help to avoid element inside the Expanded Widget to be filled inside the row, ovoid overflow inside the row
           Expanded(
             child: TextButton(
               onPressed: () {
@@ -46,5 +61,3 @@ class DicePage extends StatelessWidget {
     );
   }
 }
-
-//child: Image.asset('images/dice1.png'),
